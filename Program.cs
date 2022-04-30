9d86e27daf3b3a4e3f6b81b0a413c025e789a94b
HeryRobles <100168911+HeryRobles@users.noreply.github.com>
2022-04-30 13:26:44 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿IdentificarMes(5);
+
+static void IdentificarMes(int mes)
+{
+    string[] meses = { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" };
+    Console.WriteLine(meses[mes -1]);
+    Console.WriteLine($"El mes con el número {mes} es: {meses[(mes - 1)]}");
+
+
+}
+Console.WriteLine("Introduce el Número del mes: ");
+int mesRecibido = int.Parse(Console.ReadLine());
+IdentificarMes(mesRecibido);
+
